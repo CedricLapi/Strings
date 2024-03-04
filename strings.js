493687abab5bebafccedc48c195ce3ad4c9abce6
Cedric Lapi <lapicedric@gmail.com>
2024-03-04 17:28:27 +0100

    function getDigit added!

--- strings.js
+++ strings.js
@@ -1,6 +1,5 @@
 
-
-
+/*
 function removeBlanks(str){
 
     var array = str.split(" ")
@@ -14,3 +13,33 @@ console.log(removeBlanks(strin));
 
 var stron = " c o d i n g  i s f u n  !";
 console.log(removeBlanks(stron));
+
+
+*/
+
+
+
+
+function getDigit(str) {
+
+    var array = str.split(""); 
+
+    var result = ""; 
+
+
+    for (var i = 0; i < array.length; i++) { 
+
+        var char = array[i]; 
+
+        if (!isNaN(parseInt(char))) {
+
+            result += char; 
+        }
+    }
+
+    return parseInt(result); 
+}
+
+var strin = " c1o2d3i4ng is f5un ! ";
+console.log(getDigit(strin))
+
